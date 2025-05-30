a231241da54da48fd4fc4328ee61a478c10c02b6
Sean Gordon <seangordon@Seans-MacBook-Air.local>
2025-05-30 23:10:32 +0100

    Initial commit with main file and README

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+import zipfile
+import kaggle # Importing dataset from kaggle.
+import opendatasets as od
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
