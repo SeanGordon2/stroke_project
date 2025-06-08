8ca16ab300a68b6b7e8f95e22c378ccf4a639efc
SeanGordon2 <sean-gordon@live.com>
2025-06-08 01:15:57 +0100

    Preliminary Data Analysis

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, confusion_matrix
-import seaborn as sns; sns.set()
+import seaborn as sns;
+
+sns.set()
 import matplotlib.pyplot as plt
 import os
 import zipfile
@@ -46,7 +48,7 @@ def analysis_missing_data(df):
 def func_train_test_split(df):
     """
     Function to split into training, validation, and test data. Stratifying split based on stroke column due to low
-    stroke prevalence in dataset.
+    stroke prevalence in dataset. Hard coded to have training set = 60%, validation set = 20%, and test set = 20%.
     :param df: DataFrame
     :return: DataFrames containing training set, validation set, test set.
     """
@@ -163,10 +165,7 @@ def apply_ml_model(model, x, y):
     plt.show()
 
 
-
-
 def apply_test_logr(x_train, y_train, x_test, y_test):
-
     # 1. Scale the features
     scaler = StandardScaler()
     x_train_scaled = scaler.fit_transform(x_train)
@@ -178,16 +177,65 @@ def apply_test_logr(x_train, y_train, x_test, y_test):
     return "Finished job."
 
 
+def prelim_data_analysis(df):
+    """
+    Function to analyse data set to assess distributions, etc. Uncomment sections to view graphs.
+    :param df: DataFrame with cleaned data.
+    :return:
+    """
+    mean_stroke_incidence = df.stroke.mean()
+    print("Percentage with Stroke: ", mean_stroke_incidence)
+
+    binary_columns = ['stroke', 'hypertension', 'heart_disease', 'gender_Female', 'gender_Male', 'gender_Other',
+                      'ever_married_No', 'ever_married_Yes', 'work_type_Govt_job', 'work_type_Never_worked',
+                      'work_type_Private', 'work_type_Self-employed', 'work_type_children', 'Residence_type_Rural',
+                      'Residence_type_Urban', 'smoking_status_Unknown', 'smoking_status_formerly smoked',
+                      'smoking_status_never smoked', 'smoking_status_smokes']
+
+    # Uncomment to view charts.
+
+    # Histogram distributions of ages grouped by binary variables based on the occurrence of stroke.
+    # for col in binary_columns[1:]:
+    #     df[(df[str(col)] == 1) & (df.stroke == 0)].age.hist(bins=20, label='Non-Stroke')
+    #     df[(df[str(col)] == 1) & (df.stroke == 1)].age.hist(bins=20, label='Stroke')
+    #     plt.legend(prop={'size': 12})
+    #     plt.title(str(col))
+    #     plt.xlabel('Age (years)')
+    #     plt.ylabel('Count')
+    #     plt.show()
+
+    # Stroke rate per variable.
+    stroke_rate_per_variable = df.groupby("stroke").agg('mean')
+    print(stroke_rate_per_variable)
+
+    # Ratio of stroke incidence compared to the entire dataset.
+    ratio_stroke_rate_binary_columns = df[binary_columns].groupby("stroke").agg('mean') / mean_stroke_incidence
+    print(ratio_stroke_rate_binary_columns)
+
+    # Create frequency tables of feature vs stroke rate.
+    for col in binary_columns[1:]:  # Iterate through binary columns, skipping the stroke column.
+        print(pd.crosstab(df[col], df['stroke'], normalize='index'))
+
+    numerical_cols = [col for col in df.columns if col not in binary_columns[1:]]
+
+    # Boxplots for numerical features.
+    # for col in numerical_cols:
+    #     df.groupby('stroke')[col].describe()
+    #     sns.boxplot(data=df, x='stroke', y=str(col))
+    #     plt.title(str(col))
+    #     plt.show()
+
+
+
 def main_func(dataset_name):
     train_set, val_set, test_set = load_data_clean_and_split(dataset_name)
+    prelim_data_analysis(train_set)
 
-    x_train = train_set.loc[:, train_set.columns != "stroke"]
-    y_train = train_set["stroke"]
-
-    x_val = val_set.loc[:, val_set.columns != "stroke"]
-    y_val = val_set["stroke"]
-
-    apply_test_logr(x_train, y_train, x_val, y_val)
+    # x_train = train_set.loc[:, train_set.columns != "stroke"]
+    # y_train = train_set["stroke"]
+    # x_val = val_set.loc[:, val_set.columns != "stroke"]
+    # y_val = val_set["stroke"]
+    # apply_test_logr(x_train, y_train, x_val, y_val)
 
     return 0
 
